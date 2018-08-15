539e825ce807de39c066a5070d1008c95499d9f3
Art847proudtaxDev <arthurdimuro@gmail.com>
2018-08-16 00:28:30 +0200

    BuildingEscape12 Ray in front of the pawn that indicates the viewpoint of the player controller

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,6 +1,11 @@
 // Copyright Arthur Di Muro 2018
 
 #include "Grabber.h"
+#include "Classes/GameFramework/Actor.h" //This line must to be here on UnrealEngine4.17 or greater UE
+#include "Engine/World.h" //Necessary to use GetWorld()
+#include "GameframeWork/PlayerController.h"  //Necessary to get the player
+
+#define OUT
 
 
 // Sets default values for this component's properties
@@ -29,6 +34,19 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	FVector playerViewPointLocation;
+	FRotator playerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint( OUT playerViewPointLocation, OUT playerViewPointRotation );
+	//UE_LOG(LogTemp, Warning, TEXT("Location : %s; Position : %s"),
+	//	*playerViewPointLocation.ToString(), // X, Y and Z position
+	//	*playerViewPointRotation.ToString()); // P= vertical axis Y=horizontal axis, R= pawn rotation
+
+	// Getting player viewpoint
+	FVector lineTraceEnd = playerViewPointLocation + playerViewPointRotation.Vector() * lineLength; //Setting end point lineLength cm above the player's head
+
+	//Draw a red line to visualize orientation
+	DrawDebugLine(GetWorld(), playerViewPointLocation, lineTraceEnd, FColor(255, 255, 255), false, 0.f, 0.f, 12.f);
+	// Ray-cast out to reach distance
+	//See what we hit
 }
 
